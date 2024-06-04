0f063286b80ceafbace65123d9cfc131a4f22a64
GooDDaYYee <GooDDaYYee@gmail.com>
2024-06-04 11:29:15 +0700

    up

--- fetch_bill_details.php
+++ fetch_bill_details.php
@@ -15,6 +15,26 @@ $details_stmt->bindParam(':bill_id', $bill_id);
 $details_stmt->execute();
 $details = $details_stmt->fetchAll(PDO::FETCH_ASSOC);
 
+foreach ($details as &$detail) {
+    $auId = $detail['au_id'];
+    $query = "SELECT * FROM au_all WHERE au_id = :au_id AND au_company = 'mixed'";
+
+    $stmt = $con->prepare($query);
+    $stmt->bindParam(':au_id', $auId, PDO::PARAM_STR);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row) {
+        $detail['au_detail'] = $row['au_detail'];
+        $detail['au_type'] = $row['au_type'];
+        $detail['au_price'] = $row['au_price'];
+    } else {
+        $detail['au_detail'] = 'undefined';
+        $detail['au_type'] = 'undefined';
+        $detail['au_price'] = 'undefined';
+    }
+}
+
 $response = [
     'bill_id' => $bill['bill_id'],
     'bill_date' => $bill['bill_date'],
