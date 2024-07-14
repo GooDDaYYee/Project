38046a167cdd8f9b4663b5d2d9e61281c827955d
GooDDaYYee <GooDDaYYee@gmail.com>
2024-07-14 14:17:02 +0700

    up

--- stock/drum_delete.php
+++ stock/drum_delete.php
@@ -8,20 +8,29 @@ if (isset($_GET['drum_id'])) {
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $con->beginTransaction();
 
-        $stmtDetail = $con->prepare("DELETE FROM drum WHERE drum_id = :drum_id");
-        $stmtDetail->bindParam(':drum_id', $drum_id);
-        $stmtDetail->execute();
+        $stmtCheck = $con->prepare("SELECT drum_used FROM drum WHERE drum_id = :drum_id");
+        $stmtCheck->bindParam(':drum_id', $drum_id);
+        $stmtCheck->execute();
+        $result = $stmtCheck->fetch(PDO::FETCH_ASSOC);
 
-        $con->commit();
+        if ($result && $result['drum_used'] == 0) {
+            $stmtDetail = $con->prepare("DELETE FROM drum WHERE drum_id = :drum_id");
+            $stmtDetail->bindParam(':drum_id', $drum_id);
+            $stmtDetail->execute();
 
-        header("Location: ../index.php?page=stock/list_stock_drum");
-        exit();
+            $con->commit();
+
+            header("Location: ../index.php?page=stock/list_stock_drum");
+            exit();
+        } else {
+            $con->rollBack();
+            $drum_id = $_GET['drum_id'];
+            echo "<script type='text/javascript'>alert('ไม่สามารถลบข้อมูลได้ยังมีงานที่ใช้ drum นี้อยู่');window.location.href='../index.php?page=stock/list_stock_drum';</script>";
+        }
     } catch (PDOException $e) {
         $con->rollBack();
         echo "Error: " . $e->getMessage();
     }
 
     $con = null;
-} else {
-    echo "Invalid request.";
 }
