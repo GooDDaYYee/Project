0d71e77076e1797bcf5a1fb9f561bbe2b6232704
GooDDaYYee <GooDDaYYee@gmail.com>
2024-09-28 22:47:10 +0700

    up

--- controllers/ManageFileController.php
+++ controllers/ManageFileController.php
@@ -2,6 +2,18 @@
 require_once __DIR__ . '/BaseController.php';
 class ManageFileController extends BaseController
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Allow just admin to access this controller
+        if ($_SESSION["lv"] != 0 || $_SESSION["lv"] != 1 || $_SESSION["lv"] != 2) {
+            header("Location: index.php?page=work-report");
+            exit();
+        }
+    }
+
     public function index()
     {
         $pageTitle = 'จัดการไฟล์ - PSNK TELECOM';
